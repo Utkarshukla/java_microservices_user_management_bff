26f513dde6541025e374ed9e4ad12eb20020dafd
Utkarshukla <ram@phantasm.co.in>
2025-09-18 00:03:03 +0530

    swagger api docs

--- src/main/java/com/erp/users/ErpUserApplication.java
+++ src/main/java/com/erp/users/ErpUserApplication.java
@@ -4,8 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "ERP User Service API", 
+				version = "1.0.0", 
+				description = "API documentation for the ERP User Service",
+				contact = @Contact(
+						name = "Utkarsh Shukla"
+				),
+				license = @io.swagger.v3.oas.annotations.info.License(
+                        name = "Apache 2.0", 
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.html"
+                 )
+			)
+		)
 public class ErpUserApplication {
 
 	public static void main(String[] args) {
